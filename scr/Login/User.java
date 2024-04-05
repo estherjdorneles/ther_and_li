0bbe1f073feb7721a0a8b7da837da0597a649758
EstherJDorneles <estherjdorneles@gmail.com>
2024-04-04 21:55:24 -0300

    add user classe

---
+++ scr/Login/User.java
@@ -0,0 +1,39 @@
+package Login;
+
+import java.util.Date;
+
+public class User {
+    public String name;
+    public String email;
+    public Date birth;
+
+    public User(String name, String email, Date birth) {
+        this.name = name;
+        this.email = email;
+        this.birth = birth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getBirth() {
+        return birth;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
+}
