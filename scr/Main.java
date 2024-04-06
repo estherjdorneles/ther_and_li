2c030bad4004976547d759a7c20c7a43ad53cbc8
EstherJDorneles <estherjdorneles@gmail.com>
2024-04-05 22:08:36 -0300

    iniciando cadastro de usuário

--- scr/Main.java
+++ scr/Main.java
@@ -1,5 +1,29 @@
+import Login.User;
+
+import java.util.Date;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+
+        // variaveis de usuário
+        String nome;
+        String dataNasc;
+        String email;
+        String senha;
+
         System.out.println("Hello world and Li!");
+        System.out.println("Informe seu nome: ");
+        nome = leia.nextLine();
+        System.out.println("Data de Nascimento: ");
+        dataNasc = leia.nextLine();
+        System.out.println("Informe seu e-mail: ");
+        email = leia.nextLine();
+        System.out.println("Digite sua senha: ");
+        senha = leia.nextLine();
+        User u1 = new User(nome, email);
+        u1.toString();
     }
+
 }
